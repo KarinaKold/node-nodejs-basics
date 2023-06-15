a4baf339407e245b62c58ad4519c37b638e40b29
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2023-06-15 22:15:21 +0300

    feat: implement fs functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const errorMsg = 'FS operation failed';
+const fileToRemoveName = 'files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    const modulePath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(modulePath, fileToRemoveName);
+
+    try {
+        await rm(filePath);
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const errorMsg = 'FS operation failed';
+const fileToReadName = 'files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const modulePath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(modulePath, fileToReadName);
+
+    try {
+        const contents = await readFile(filePath, 'utf-8');
+        console.log(contents);
+    } catch (err) {
+        throw new Error(errorMsg);
+    }
 };
 
 await read();
