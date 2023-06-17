d7b3d30becf80b71bed7af6ae590800c4ce7fb4d
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-17 22:55:25 +0300

    feat: implement functions compresses file & decompresses back using zlib and Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+const filename = './files/fileToCompress.txt';
+const zipFilename = './files/archive.gz';
+const fileURL = new URL(filename, import.meta.url);
+const zipFileURL = new URL(zipFilename, import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(createReadStream(fileURL), createGzip(), createWriteStream(zipFileURL));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { pipeline } from 'node:stream/promises';
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+const filename = './files/fileToCompress.txt';
+const zipFilename = './files/archive.gz';
+const fileURL = new URL(filename, import.meta.url);
+const zipFileURL = new URL(zipFilename, import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(createReadStream(zipFileURL), createGunzip(), createWriteStream(fileURL));
 };
 
 await decompress();
