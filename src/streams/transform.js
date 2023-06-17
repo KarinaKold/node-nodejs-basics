c41fd3c3812247df048289ab215d99aee1c3727b
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-17 22:38:29 +0300

    feat: implement transform.js function using Transform Stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransformStreem = new Transform({
+        transform(chunk, _, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    })
+    stdin.pipe(reverseTransformStreem).pipe(stdout);
 };
 
 await transform();
