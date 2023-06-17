01a8bea4dbe90c94a45ae8fbf933cf766fb4731e
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-17 13:24:19 +0300

    fix: reverse string to the same order

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,7 +2,7 @@ const prefix = 'RSS_';
 
 const parseEnv = () => {
     const keys = Object.entries(process.env).filter(([key, _]) => key.includes(prefix));
-    const variables = keys.map((entry) => entry.join('=')).join('; ');
+    const variables = keys.map((entry) => entry.join('=')).reverse().join('; ');
     console.log(variables);
 };
 
