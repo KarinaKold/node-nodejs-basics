29ea8123a82f871d1cc437be8463a0fdd79c7b13
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-17 22:15:36 +0300

    feat: implement read.js function using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const filename = './files/fileToRead.txt';
+const fileURL = new URL(filename, import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    const fileReadStream = createReadStream(fileURL);
+    await pipeline(fileReadStream, process.stdout);
 };
 
 await read();
