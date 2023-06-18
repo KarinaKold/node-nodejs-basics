509b27a8c20b955fd83234152901ea793603a895
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-18 14:30:03 +0300

    feat: implement function that creates number of worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const workerFile = './src/wt/worker.js';
+
+let initWorkerData = 10;
+const workersNum = cpus();
+
 const performCalculations = async () => {
-    // Write your code here
+    const myPromises = workersNum.map((_, i) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerFile, { workerData: initWorkerData + i } );
+    
+            worker.on('message', (result) => resolve({ status: 'resolved', data: result }));
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+        })
+    })
+    
+    const results = await Promise.all(myPromises);
+    console.log(results);
 };
 
 await performCalculations();
