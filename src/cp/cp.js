3ffeef97340ee28f4ab7ea3ba62f9007e7dc5354
Karina Koldaeva <karina.mila@yandex.ru>
2023-06-18 14:45:09 +0300

    feat: implement function spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from 'node:child_process';
+
+const filename = './files/script.js';
+const fileURL = new URL(filename, import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(fileURL, args, { silent: true });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const someArgument1 = 'someArgument1';
+const someArgument2 = 'someArgument2';
+spawnChildProcess( /*[someArgument1, someArgument2]*/ );
